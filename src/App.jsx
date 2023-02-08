d02af7d44020fa3a95b43a1a8b3ff65e976b9870
Adriana <drib.yamashita@gmail.com>
2023-02-08 09:19:45 -0300

    Atualização da aula 2 e 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
-import { Button } from "./components/button";
-import { Text } from "./components/text";
-import { Avatar } from "./components/avatar";
-import { Header } from "./components/header";
-import { NavItem } from "./components/nav-item";
-import { Navbar } from "./components/navbar";;
-import { Title } from "./components/title";
-
-const items = [
-  "Página Inicial",
-  "Pesquisa",
-  "Explorar",
-  "Reels",
-  "Mensagens",
-  "Notificações",
-  "Criar",
-  "Perfil",
-  "Mais",
-];
+import React, {useEffect} from "react";
+import {Header} from "./components/header";
+import {Highlights} from "./components/highlights/highlights";
+import {Navbar} from "./components/navbar";
+import {fetcher} from "./services";
+import {Grid, GridItem} from "./ui/grid";
 
 function App() {
+  useEffect(() => {
+    const makeRequest = async () => {
+      const response = await fetcher("photos");
+      console.log(response);
+    };
 
-    return (
-      <>
-        <Navbar>
-          <Title />
-          {items.map((item)=> (
-            <NavItem key={item} text={item}/>
-          ))}
-        </Navbar>
-        <Header>
-          <Avatar />
-          <Text bold={true}>@adatech</Text>
-          <Button>Seguindo</Button>
-          <Button>Enviar mensagem</Button>
-          <Text>211 publicações</Text>
-          <Text>44,2 mil seguidores</Text>
-          <Text>2 seguindo</Text>
-        </Header>
-      </>
-    );
+    makeRequest();
+  }, []);
+  return (
+    <Grid>
+      <GridItem>
+        <Navbar />
+      </GridItem>
+      <GridItem>
+        <Header />
+        <Highlights />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
